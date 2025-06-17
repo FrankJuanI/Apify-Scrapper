17dc24cb83aabbd5e96b84b50f5382c8a8bf341e
Juan Frank <juan.frank@gm2dev.com>
2025-06-17 17:37:49 -0300

    feat: use input Ids instead of hardcoded ids

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ await Actor.init();
 interface Input {
     companyIds: string[];
 }
+
 const {
-    companyIds = ['4821421', '23852', '25192'],
+    companyIds = [],
 } = (await Actor.getInput<Input>()) ?? {};
 
 // Creamos la cola de requests
